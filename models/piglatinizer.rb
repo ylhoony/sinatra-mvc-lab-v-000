78dc13cedc1c6c69b8b41f3b671c2ae360cfee56
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 19:52:42 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,7 @@ class PigLatinizer
 
   def to_pig_latin(sentence)
     sentence_arr = sentence.slice(" ")
-    sentence_arr.map do |word|
+    sentence_arr.collect do |word|
       piglatinize(word)
     end.join(" ")
   end
