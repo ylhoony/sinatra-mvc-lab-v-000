70451294ff3943518b96e92e5d5678fe651699c2
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 19:22:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinizer
   # attr_accessor :phrase
 
   def piglatinize(word)
-    phrase.slice(1, phrase.size-1).concat(phrase[0],"way")
+    word.slice(1, phrase.size-1).concat(phrase[0],"way")
   end
 
   def to_pig_latin
