5e83d1890456e1987b386ed4790d282fcd9c07ea
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 19:59:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     user_input = params[:user_input]
-    @piglatinized = PigLatinizer.new
+    piglatinized = PigLatinizer.new
+    @result = piglatinized.to_pig_latin(user_input)
 
     erb :piglatinize
   end
