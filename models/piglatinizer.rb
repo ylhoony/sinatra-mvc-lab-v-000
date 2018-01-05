a925bde9a62f8294f86cda9f107eb075d07cde03
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 19:48:13 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,16 +3,13 @@ class PigLatinizer
 
   def piglatinize(word)
     word_arr = word.split("")
+    consonant_index = word_arr.index {|char| char.match(/\A([aeiou]|[AEIOU])/)}
 
-    if word_arr.index {|char| char.match(/\A([aeiou]|[AEIOU])/)} == 0
-        word.concat("way")
-    binding.pry
-
-    # if word.chr.match(/\A([aeiou]|[AEIOU])/)
-    #   word.concat("way")
-    # else
-    #   word.slice(1, word.size-1).concat(word[0]).concat("ay")
-    # end
+    if consonant_index == 0
+      word.concat("way")
+    else
+      word.slice(1, word.size-1).concat(word[0]).concat("ay")
+    end
   end
 
   def to_pig_latin
